3aeb6df059bffc050d108432cbc0a71d42a52a76
antonio.torre <antonio.torre@pagopa.it>
2025-11-07 13:43:32 +0100

    Added X-Trace-Id response header + Added traceId field on ErrorDTO

--- src/test/java/it/gov/pagopa/pu/citizen/exception/ControllerExceptionHandlerTest.java
+++ src/test/java/it/gov/pagopa/pu/citizen/exception/ControllerExceptionHandlerTest.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.pu.citizen.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.pu.citizen.config.json.JsonConfig;
+import it.gov.pagopa.pu.citizen.utils.UtilitiesTest;
 import jakarta.servlet.ServletException;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
@@ -13,6 +14,8 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.validator.internal.engine.ConstraintViolationImpl;
 import org.hibernate.validator.internal.engine.path.PathImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -86,6 +89,16 @@ class ControllerExceptionHandlerTest {
         private LocalDateTime dateTimeField;
     }
 
+    private final String traceId = "TRACEID";
+    @BeforeEach
+    void setTraceId(){
+      UtilitiesTest.setTraceId(traceId);
+    }
+    @AfterEach
+    void clearTraceId(){
+      UtilitiesTest.clearTraceIdContext();
+    }
+
     private ResultActions performRequest(String data, MediaType accept) throws Exception {
         return performRequest(data, accept, objectMapper.writeValueAsString(ControllerExceptionHandlerTest.BODY));
     }
@@ -110,7 +123,8 @@ class ControllerExceptionHandlerTest {
         performRequest(null, MediaType.APPLICATION_JSON)
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Required request parameter 'data' for method parameter type String is not present"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Required request parameter 'data' for method parameter type String is not present"))
+          .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
 
     }
 
@@ -121,7 +135,8 @@ class ControllerExceptionHandlerTest {
         performRequest(DATA, MediaType.APPLICATION_JSON)
                 .andExpect(MockMvcResultMatchers.status().isInternalServerError())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("GENERIC_ERROR"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"))
+          .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
     }
 
     @Test
@@ -132,7 +147,8 @@ class ControllerExceptionHandlerTest {
         performRequest(DATA, MediaType.APPLICATION_JSON)
                 .andExpect(MockMvcResultMatchers.status().isInternalServerError())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("GENERIC_ERROR"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"))
+          .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
     }
 
     @Test
@@ -140,7 +156,8 @@ class ControllerExceptionHandlerTest {
         performRequest(DATA, MediaType.parseMediaType("application/hal+json"))
                 .andExpect(MockMvcResultMatchers.status().isNotAcceptable())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No acceptable representation"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No acceptable representation"))
+          .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
     }
 
   @Test
@@ -148,7 +165,8 @@ class ControllerExceptionHandlerTest {
     mockMvc.perform(MockMvcRequestBuilders.post("/NOTEXISTENTURL"))
       .andExpect(MockMvcResultMatchers.status().isNotFound())
       .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("NOT_FOUND"))
-      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No static resource NOTEXISTENTURL."));
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No static resource NOTEXISTENTURL."))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
   }
 
     @Test
@@ -156,7 +174,8 @@ class ControllerExceptionHandlerTest {
         performRequest(DATA, MediaType.APPLICATION_JSON, null)
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Required request body is missing"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Required request body is missing"))
+          .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
     }
 
     @Test
@@ -165,7 +184,8 @@ class ControllerExceptionHandlerTest {
                 "{\"notRequiredField\":\"notRequired\",\"lowerCaseAlphabeticField\":\"ABC\"}")
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Invalid request content. lowerCaseAlphabeticField: must match \"[a-z]+\"; requiredField: must not be null"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Invalid request content. lowerCaseAlphabeticField: must match \"[a-z]+\"; requiredField: must not be null"))
+          .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
     }
 
     @Test
@@ -185,7 +205,8 @@ class ControllerExceptionHandlerTest {
         performRequest(DATA, MediaType.APPLICATION_JSON)
                 .andExpect(MockMvcResultMatchers.status().isInternalServerError())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("GENERIC_ERROR"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("500 INTERNAL_SERVER_ERROR \"Error\""));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("500 INTERNAL_SERVER_ERROR \"Error\""))
+          .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
     }
 
     private final ConstraintViolationException constraintViolationException = new ConstraintViolationException("Error", Set.of(ConstraintViolationImpl.forParameterValidation(
@@ -199,7 +220,8 @@ class ControllerExceptionHandlerTest {
         performRequest(DATA, MediaType.APPLICATION_JSON)
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Invalid request content. fieldName: resolved message"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Invalid request content. fieldName: resolved message"))
+          .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
     }
 
   @Test
@@ -219,7 +241,8 @@ class ControllerExceptionHandlerTest {
     performRequest(DATA, MediaType.APPLICATION_JSON)
       .andExpect(MockMvcResultMatchers.status().isForbidden())
       .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("GENERIC_ERROR"))
-      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(HttpStatus.FORBIDDEN.value()+" Error"));
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(HttpStatus.FORBIDDEN.value()+" Error"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
   }
 
   @Test
@@ -229,7 +252,8 @@ class ControllerExceptionHandlerTest {
     performRequest(DATA, MediaType.APPLICATION_JSON)
         .andExpect(MockMvcResultMatchers.status().isInternalServerError())
         .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("GENERIC_ERROR"))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
   }
 
   @Test
@@ -239,7 +263,8 @@ class ControllerExceptionHandlerTest {
     performRequest(DATA, MediaType.APPLICATION_JSON)
         .andExpect(MockMvcResultMatchers.status().isForbidden())
         .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("FORBIDDEN"))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
   }
 
   @Test
@@ -249,7 +274,8 @@ class ControllerExceptionHandlerTest {
     performRequest(DATA, MediaType.APPLICATION_JSON)
         .andExpect(MockMvcResultMatchers.status().isConflict())
         .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("CONFLICT"))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
   }
 
   @Test
@@ -259,6 +285,7 @@ class ControllerExceptionHandlerTest {
     performRequest(DATA, MediaType.APPLICATION_JSON)
         .andExpect(MockMvcResultMatchers.status().isBadRequest())
         .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("BAD_REQUEST"))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
   }
 }
