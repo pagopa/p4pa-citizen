83dc4a0c689fb14c0628e33db248a43439a04ed6
antonio.torre <antonio.torre@pagopa.it>
2025-12-19 16:36:51 +0100

    fix: Setting default TimeZone on main

--- src/main/java/it/gov/pagopa/pu/citizen/PuCitizenApplication.java
+++ src/main/java/it/gov/pagopa/pu/citizen/PuCitizenApplication.java
@@ -1,14 +1,18 @@
 package it.gov.pagopa.pu.citizen;
 
+import it.gov.pagopa.pu.citizen.utils.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
 public class PuCitizenApplication {
 
 	public static void main(String[] args) {
+    TimeZone.setDefault(Constants.DEFAULT_TIMEZONE);
 		SpringApplication.run(PuCitizenApplication.class, args);
 	}
 
