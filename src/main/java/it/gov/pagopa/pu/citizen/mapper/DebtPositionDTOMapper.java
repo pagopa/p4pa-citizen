4cfa641197be5d92943d8c3d0f149b3fe37f820b
MScarsella <190512124+MScarsella@users.noreply.github.com>
2026-01-22 15:43:26 +0100

    fix: P4PU-1104 handle remittanceInformation in createSpontaneousDebtPosition API (#50)
    Co-authored-by: mscarsel <marco.scarsella@nttdata.com>

--- src/main/java/it/gov/pagopa/pu/citizen/mapper/DebtPositionDTOMapper.java
+++ src/main/java/it/gov/pagopa/pu/citizen/mapper/DebtPositionDTOMapper.java
@@ -3,12 +3,15 @@ package it.gov.pagopa.pu.citizen.mapper;
 import it.gov.pagopa.pu.citizen.dto.generated.DebtPositionRequestDTO;
 import it.gov.pagopa.pu.citizen.dto.generated.InstallmentRequestDTO;
 import it.gov.pagopa.pu.citizen.dto.generated.PaymentOptionRequestDTO;
+import it.gov.pagopa.pu.citizen.utils.DebtPositionConstants;
 import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
 import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentDTO;
 import it.gov.pagopa.pu.debtpositions.dto.generated.PaymentOptionDTO;
+import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface DebtPositionDTOMapper {
@@ -27,6 +30,14 @@ public interface DebtPositionDTOMapper {
   @Mapping(target = "status", constant = "UNPAID")
   @Mapping(target = "switchToExpired", constant = "true")
   @Mapping(target = "dueDate", expression = "java(expirationDays != null ? java.time.LocalDate.now().plusDays(expirationDays) : java.time.LocalDate.now())")
+  @Mapping(target = "originalRemittanceInformation", source = "remittanceInformation")
+  @Mapping(target = "remittanceInformation", source = "userRemittanceInformation", qualifiedByName = "userRemittanceInformation")
   InstallmentDTO mapSpontaneousInstallmentDTO(InstallmentRequestDTO installmentRequestDTO, @Context Integer expirationDays);
 
+  @Named("userRemittanceInformation")
+  default String mapUserRemittanceInformation(String userRemittanceInformation){
+    return StringUtils.isNotBlank(userRemittanceInformation)?
+      DebtPositionConstants.INSTALLMENT_REMITTANCE_INFORMATION_PLACEHOLDER + " " + userRemittanceInformation
+      : DebtPositionConstants.INSTALLMENT_REMITTANCE_INFORMATION_PLACEHOLDER;
+  }
 }
