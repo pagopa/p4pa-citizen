b43c707c12cc55817cdbb53e1218e07fa0580ec3
mscarsel <190512124+MScarsella@users.noreply.github.com>
2026-02-25 17:39:56 +0100

    [P4PU-1262] add cie connector and AuthnService.getAccessToken

---
+++ src/main/java/it/gov/pagopa/pu/citizen/connector/cie/client/CieDebtPositionClient.java
@@ -0,0 +1,22 @@
+package it.gov.pagopa.pu.citizen.connector.cie.client;
+
+import it.gov.pagopa.pu.cie.dto.generated.DebtPositionCieRequestDTO;
+import it.gov.pagopa.pu.citizen.connector.cie.config.CieApisHolder;
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class CieDebtPositionClient {
+
+  private final CieApisHolder cieApisHolder;
+
+  public CieDebtPositionClient(CieApisHolder cieApisHolder) {
+    this.cieApisHolder = cieApisHolder;
+  }
+
+  public DebtPositionDTO createDebtPositionCie(DebtPositionCieRequestDTO debtPositionCieRequestDTO, String accessToken) {
+    return cieApisHolder.getDebtPositionApi(accessToken).createDebtPositionCie(debtPositionCieRequestDTO);
+  }
+}
---
+++ src/test/java/it/gov/pagopa/pu/citizen/connector/cie/client/CieDebtPositionClientTest.java
@@ -0,0 +1,60 @@
+package it.gov.pagopa.pu.citizen.connector.cie.client;
+
+import it.gov.pagopa.pu.cie.controller.generated.DebtPositionApi;
+import it.gov.pagopa.pu.cie.dto.generated.DebtPositionCieRequestDTO;
+import it.gov.pagopa.pu.citizen.connector.cie.config.CieApisHolder;
+import it.gov.pagopa.pu.citizen.utils.TestUtils;
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.co.jemos.podam.api.PodamFactory;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CieDebtPositionClientTest {
+
+  @Mock
+  private CieApisHolder cieApisHolderMock;
+  @Mock
+  private DebtPositionApi debtPositionApiMock;
+
+  private CieDebtPositionClient cieDebtPositionClient;
+
+  private final PodamFactory podamFactory = TestUtils.getPodamFactory();
+
+  @BeforeEach
+  void setUp() {
+    cieDebtPositionClient = new CieDebtPositionClient(cieApisHolderMock);
+  }
+
+  @AfterEach
+  void verifyNoMoreInteractions() {
+    Mockito.verifyNoMoreInteractions(
+      cieApisHolderMock,
+      debtPositionApiMock
+    );
+  }
+
+  @Test
+  void whenCreateDebtPositionThenInvokeWithAccessToken() {
+    DebtPositionCieRequestDTO debtPositionCieRequestDTO = podamFactory.manufacturePojo(DebtPositionCieRequestDTO.class);
+    String accessToken = "ACCESSTOKEN";
+    DebtPositionDTO expectedResult = podamFactory.manufacturePojo(DebtPositionDTO.class);
+
+    when(cieApisHolderMock.getDebtPositionApi(accessToken))
+      .thenReturn(debtPositionApiMock);
+    when(debtPositionApiMock.createDebtPositionCie(debtPositionCieRequestDTO))
+      .thenReturn(expectedResult);
+
+    DebtPositionDTO result = cieDebtPositionClient.createDebtPositionCie(debtPositionCieRequestDTO, accessToken);
+
+    Assertions.assertSame(expectedResult, result);
+  }
+}
