49047838430595310b7ec876c670c19f0320170a
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2026-03-06 00:52:09 +0100

    fix: Fix CVE (#73)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ import java.util.*
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.0"
+  id("org.springframework.boot") version "4.0.3"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.2.1.6560"
+  id("org.sonarqube") version "7.2.3.7755"
   id("com.github.ben-manes.versions") version "0.53.0"
-  id("org.openapi.generator") version "7.17.0"
-  id("com.gorylenko.gradle-git-properties") version "2.5.4"
+  id("org.openapi.generator") version "7.20.0"
+  id("com.gorylenko.gradle-git-properties") version "2.5.7"
   id("org.ajoberstar.grgit") version "5.3.2"
-  id("com.github.jk1.dependency-license-report") version "3.0.1"
+  id("com.github.jk1.dependency-license-report") version "3.1.1"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -50,18 +50,23 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.0"
+val springDocOpenApiVersion = "3.0.2"
 val janinoVersion = "3.1.12"
-val openApiToolsVersion = "0.2.8"
-val micrometerVersion = "1.6.1"
-val httpClientVersion = "5.5.1"
+val openApiToolsVersion = "0.2.9"
+val micrometerVersion = "1.6.3"
+val httpClientVersion = "5.6"
+val httpCoreVersion = "5.4.1"
 val podamVersion = "8.0.2.RELEASE"
-val javaJwtVersion = "4.5.0"
+val javaJwtVersion = "4.5.1"
 val jwksRsaVersion = "0.23.0"
 val bouncycastleVersion = "1.83"
 val nimbusVersion = "10.5"
 val mapStructVersion = "1.6.3"
 val commonsLang3Version = "3.20.0"
+
+// fix cve
+val jackson2CoreVersion = "2.21.1"
+val jackson3CoreVersion = "3.1.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -79,6 +84,7 @@ dependencies {
   implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
   implementation("org.apache.httpcomponents.client5:httpclient5:$httpClientVersion")
+  implementation("org.apache.httpcomponents.core5:httpcore5:$httpCoreVersion")
   implementation("org.springframework.data:spring-data-commons")
   implementation("org.mapstruct:mapstruct:${mapStructVersion}")
 
@@ -87,6 +93,10 @@ dependencies {
   implementation("com.auth0:java-jwt:${javaJwtVersion}")
   implementation("com.auth0:jwks-rsa:${jwksRsaVersion}")
   implementation("org.bouncycastle:bcprov-jdk18on:${bouncycastleVersion}")
+
+  // CVE fix
+  implementation("tools.jackson.core:jackson-core:$jackson3CoreVersion")
+  implementation("com.fasterxml.jackson.core:jackson-core:$jackson2CoreVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
