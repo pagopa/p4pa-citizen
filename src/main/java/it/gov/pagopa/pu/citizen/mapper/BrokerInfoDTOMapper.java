c77d7b624ba5ed7a4e85f29457d732d4b028ad52
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2026-04-28 16:03:15 +0200

    P4PU-1404 Updates OpenAPI spec to add favicon and refactor business logic

--- src/main/java/it/gov/pagopa/pu/citizen/mapper/BrokerInfoDTOMapper.java
+++ src/main/java/it/gov/pagopa/pu/citizen/mapper/BrokerInfoDTOMapper.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.pu.citizen.mapper;
 
 import it.gov.pagopa.pu.citizen.dto.generated.BrokerInfoDTO;
+import it.gov.pagopa.pu.organization.dto.generated.BrokerConfiguration;
 import it.gov.pagopa.pu.organization.dto.generated.Organization;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -8,16 +9,17 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface BrokerInfoDTOMapper {
 
+  @Mapping(target = "brokerId", source = "organization.brokerId")
   @Mapping(target = "brokerLogo", source = "organization.orgLogo")
   @Mapping(target = "brokerName", source = "organization.orgName")
   @Mapping(target = "brokerFiscalCode", source = "organization.orgFiscalCode")
-  @Mapping(target = "config", source = "arpuConfig")
-  BrokerInfoDTO innerMap(Organization organization, String externalId, String arpuConfig);
+  @Mapping(target = "config", source = "brokerConfiguration.arpuConfig")
+  BrokerInfoDTO innerMap(Organization organization, String externalId, BrokerConfiguration brokerConfiguration);
 
-  default BrokerInfoDTO map(Organization organization, String externalId, String arpuConfig) {
+  default BrokerInfoDTO map(Organization organization, String externalId, BrokerConfiguration brokerConfiguration) {
     if (organization == null) {
       return null;
     }
-    return innerMap(organization, externalId, arpuConfig);
+    return innerMap(organization, externalId, brokerConfiguration);
   }
 }
