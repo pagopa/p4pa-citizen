5eb2ff5ec6e6438594296a66ebe45461ba1cf0ed
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2026-09-01 17:34:54 +0200

    fix: Fix CVE (#107)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ import java.util.*
 
 plugins {
   java
-  id("org.springframework.boot") version "4.1.0"
+  id("org.springframework.boot") version "4.1.1"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.3.1.8318"
+  id("org.sonarqube") version "7.4.0.8496"
   id("com.github.ben-manes.versions") version "0.54.0"
-  id("org.openapi.generator") version "7.23.0"
+  id("org.openapi.generator") version "7.25.0"
   id("com.gorylenko.gradle-git-properties") version "4.0.1"
   id("org.ajoberstar.grgit") version "5.3.2"
   id("com.github.jk1.dependency-license-report") version "3.1.4"
@@ -50,17 +50,17 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.3"
-val openApiToolsVersion = "0.2.10"
-val micrometerVersion = "1.7.0"
+val springDocOpenApiVersion = "3.1.0"
+val openApiToolsVersion = "0.2.11"
+val micrometerVersion = "1.7.1"
 val httpClientVersion = "5.6.4"
 val httpCoreVersion = "5.4.3"
 val kafkaAppender = "0.2.0-RC2"
-val lz4JavaVersion = "1.11.1"
+val lz4JavaVersion = "1.11.2"
 val podamVersion = "8.0.2.RELEASE"
-val javaJwtVersion = "4.5.2"
+val javaJwtVersion = "4.6.0"
 val jwksRsaVersion = "0.24.1"
-val bouncycastleVersion = "1.84"
+val bouncycastleVersion = "1.85.2"
 val nimbusJoseJwtVersion = "10.9.1"
 val mapStructVersion = "1.6.3"
 val commonsLang3Version = "3.20.0"
@@ -87,13 +87,13 @@ dependencies {
   }
   implementation("at.yawk.lz4:lz4-java:$lz4JavaVersion")
   implementation("org.springframework.data:spring-data-commons")
-  implementation("org.mapstruct:mapstruct:${mapStructVersion}")
+  implementation("org.mapstruct:mapstruct:$mapStructVersion")
 
   // validation token jwt
-  implementation("com.nimbusds:nimbus-jose-jwt:${nimbusJoseJwtVersion}")
-  implementation("com.auth0:java-jwt:${javaJwtVersion}")
-  implementation("com.auth0:jwks-rsa:${jwksRsaVersion}")
-  implementation("org.bouncycastle:bcprov-jdk18on:${bouncycastleVersion}")
+  implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")
+  implementation("com.auth0:java-jwt:$javaJwtVersion")
+  implementation("com.auth0:jwks-rsa:$jwksRsaVersion")
+  implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
@@ -106,7 +106,7 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-security-test")
   testImplementation("org.mockito:mockito-core")
   testImplementation("org.projectlombok:lombok")
-  testImplementation("uk.co.jemos.podam:podam:${podamVersion}")
+  testImplementation("uk.co.jemos.podam:podam:$podamVersion")
 }
 
 tasks.withType<Test> {
